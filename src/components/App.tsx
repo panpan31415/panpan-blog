a917e82a4e70978cf8e69da42e163f9bb0e6203d
panpan <panpan31415@gmail.com>
2024-03-04 17:20:53 +0100

    Refactor App component and add App.scss

---
+++ src/components/App.tsx
@@ -0,0 +1,29 @@
+import { Fragment } from "react/jsx-runtime";
+import "./App.scss";
+import NavigationArea from "./NavigationArea";
+import MainContent from "./MainContent";
+import NavigationContext from "./NavigationContext";
+import { useState } from "react";
+
+function App() {
+  const [activeRoute, setActiveRoute] = useState("about");
+  return (
+    <Fragment>
+      <div className="app-wrapper">
+        <div className="app">
+          <NavigationContext.Provider
+            value={{
+              activeRoute,
+              setActiveRoute,
+            }}
+          >
+            <NavigationArea />
+            <MainContent />
+          </NavigationContext.Provider>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
