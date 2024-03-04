5f3dd5b2bd549b9c2b311346fbdec5a4c06d9063
panpan <panpan31415@gmail.com>
2024-03-05 00:59:53 +0100

    Add styles and content to home section

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -6,10 +6,28 @@ export default function MainContent() {
   const size = useComponentSize(ref);
   return (
     <main className="main" ref={ref}>
-      <div className="content-frame" style={{ width: size.width }}></div>
+      <div className="content-frame" style={{ width: size.width }} />
 
-      <div className="children--1" id={"home"}>
-        1
+      <section className="home-section" id={"home"}>
+        <div className="home-section__intro">
+          <p className="home-section__hello">Hi, I'm</p>
+          <h1 className="home-section__name">
+            Panpan
+            <br />
+            Zhang
+          </h1>
+          <div className="home-section__divider" />
+          <p className="home-section__position">Frontend Developer</p>
+        </div>
+        <div className="home-section__profile"></div>
+      </section>
+      <div className="home-section__links">
+        <a className="home-section__links--left" href="/">
+          DOWNLOAD RESUME
+        </a>
+        <a className="home-section__links--right" href="/">
+          View Profile
+        </a>
       </div>
 
       <div className="children--2" id={"about"}>
