9839364cf76545555c7e1613899a40d25b0504cb
panpan <panpan31415@gmail.com>
2024-03-04 13:30:40 +0100

    Refactor App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import NavigationArea from "./components/NavigationArea";
 function App() {
   return (
     <Fragment>
-      <div className="app">
-        <NavigationArea />
-        <main className="main">
-          <div className="children--1">1</div>
-          <div className="children--2">2</div>
-          <div className="children--3">3</div>
-        </main>
+      <div className="app-wrapper">
+        <div className="app">
+          <NavigationArea />
+          <main className="main">
+            <div className="children--1">1</div>
+            <div className="children--2">2</div>
+            <div className="children--3">3</div>
+          </main>
+        </div>
       </div>
     </Fragment>
   );
